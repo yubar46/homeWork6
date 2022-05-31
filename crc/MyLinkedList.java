3e60cdb802471a49e6cf72f4f31d61f8d7cca9d4
yubar46 <yubar4.6@gmail.com;git config --global user.email yubar4.6@gmail.com>
2022-05-31 10:14:09 +0430

    add method getHead in class MyLinkedList add method isEmpty in class MyLinkedList add method addLast in class MyLinkedList

--- crc/MyLinkedList.java
+++ crc/MyLinkedList.java
@@ -13,5 +13,38 @@ public class MyLinkedList<T> {
         this.head.value = value;
     }
 
+    public Node<T> getHead() {  //لارم مشه پاک میشه
+        return head;
+    }
+
+    public boolean isEmpty(){
+        return head==null;
+    }
+
+    public void print(){
+        Node node = this.head;
+        while (node!=null){
+            System.out.println(node.value.toString());
+            node= node.next;
+        }
+    }
+
+    public void addLast(T value){
+
+        if (isEmpty()){
+            Node lastNode = new Node(value);
+            head = lastNode;
+        }else {
+
+            Node lastNode = head;
+            while (lastNode.next!=null){
+                lastNode = lastNode.next;
+            }
+            Node newNode = new Node(value);
+            lastNode.next=newNode;
+        }
+
+    }
+
 
 }
